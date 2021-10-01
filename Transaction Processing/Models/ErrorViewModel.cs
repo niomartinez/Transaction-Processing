761e85f2833004e43b311c881c1b6a2493f461cc
Nio Martinez <niosupetranmartinez@gmail.com>
2021-10-01 17:28:55 +0800

    Add project files.

---
+++ Transaction Processing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Transaction_Processing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
